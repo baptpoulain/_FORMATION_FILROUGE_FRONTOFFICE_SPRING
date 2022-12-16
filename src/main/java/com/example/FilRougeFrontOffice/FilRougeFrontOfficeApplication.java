f5b5c6a8d852e5acbc76392ed36cb38b2591955b
baptpoulain <bapt.poulain@outlook.com>
2022-12-16 16:20:53 +0100

    Initial commit

---
+++ src/main/java/com/example/FilRougeFrontOffice/FilRougeFrontOfficeApplication.java
@@ -0,0 +1,13 @@
+package com.example.FilRougeFrontOffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilRougeFrontOfficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilRougeFrontOfficeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FilRougeFrontOffice/FilRougeFrontOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FilRougeFrontOffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilRougeFrontOfficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
